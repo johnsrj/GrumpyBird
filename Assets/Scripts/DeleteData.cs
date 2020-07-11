19fff375ebfb6fbe73741f1773b80a949199e6cd
John de Souza Junior <37567335+johnsrj@users.noreply.github.com>
2020-07-10 20:54:17 -0400

    Add files via upload

---
+++ Assets/Scripts/DeleteData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class DeleteData : MonoBehaviour
+{
+    private Button button { get { return GetComponent<Button>(); } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(() => DelData());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DelData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    public GameObject highScoreIndicator;
+
+    private void Start(){
+        Time.timeScale = 1;
+        //gameOverCanvas.SetActive(false);
+    }
+
+    public void GameOver(){
+        gameOverCanvas.SetActive(true);
+        if (Score.hi < Score.score){
+            Score.hi = Score.score;
+            highScoreIndicator.SetActive(true);
+            PlayerPrefs.SetInt("highscore", Score.hi);
+        }
+        //Debug.Log(Score.hi);
+        Time.timeScale = 0;
+    }
+
+    public void Replay(){
+        SceneManager.LoadScene(1);  
+    }
+}
